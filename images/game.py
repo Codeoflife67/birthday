a61150d238d6dea6d13e3db990c4237a458e14bb
Codeoflife67 <144304361+Codeoflife67@users.noreply.github.com>
2023-09-07 06:50:48 +0200

    Add files via upload

---
+++ images/game.py
@@ -0,0 +1,13 @@
+import random 
+
+top_of_range = input("type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print("Please type a number larger than 0 next time.")
+        quit()
+else:                                          
+    print ("print type a number next time")
+random_number = random.randint(0,11)
